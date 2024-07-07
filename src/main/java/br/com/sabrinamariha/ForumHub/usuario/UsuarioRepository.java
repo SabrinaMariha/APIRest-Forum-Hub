0a3e81cc12ff827d2fdc864aa6012b4474453ca4
Sabrina Mariha <sabrinammgs@gmail.com>
2024-07-07 14:30:22 -0300

    Query para verificar se há usuários cadastrados com o mesmo nome ou email enviados na requisição de cadastro.

--- src/main/java/br/com/sabrinamariha/ForumHub/usuario/UsuarioRepository.java
+++ src/main/java/br/com/sabrinamariha/ForumHub/usuario/UsuarioRepository.java
@@ -1,7 +1,18 @@
 package br.com.sabrinamariha.ForumHub.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Usuario findByEmail(String email);
+    @Query("""
+            SELECT CASE WHEN COUNT(u) > 0 THEN FALSE ELSE TRUE END FROM Usuario u 
+            WHERE 
+            u.email = :email
+            OR 
+            u.nome = :nome
+            """)
+    boolean validacaoDuplicidadeUsuario(@Param("email") String email, @Param("nome") String nome);
+
 }
