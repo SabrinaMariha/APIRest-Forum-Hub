5a134e8e3e7d74da95d8540357ef06434835dfc5
Sabrina Mariha <sabrinammgs@gmail.com>
2024-07-08 14:47:14 -0300

    Configurações de cors.

--- src/main/java/br/com/sabrinamariha/ForumHub/security/SecurityConfigurations.java
+++ src/main/java/br/com/sabrinamariha/ForumHub/security/SecurityConfigurations.java
@@ -13,36 +13,52 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
     @Autowired
     private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/login/**","/usuarios/cadastro").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/topicos").permitAll()  // Permitir acesso ao endpoint /topicos
                         .requestMatchers("/v3/api-docs/**","swagger-ui.html","/swagger-ui/**").permitAll()
-                        //.requestMatchers(HttpMethod.DELETE,"/medicos").hasRole("ADMIN")// Permitir acesso ao endpoint /login
-                        //.requestMatchers(HttpMethod.DELETE,"/pacientes").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .cors().and()  // Ativar CORS
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
-
         return configuration.getAuthenticationManager();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-}
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5501", "http://localhost:8080"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
